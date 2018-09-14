21d0c25678b4a974f146f8ade7ab822e021b9045
Cody S <sheridan.codyj@gmail.com>
2018-09-14 05:23:16 -0400

    Completed

--- 03-Arrays/arrayPractice.js
+++ 03-Arrays/arrayPractice.js
@@ -3,24 +3,23 @@ var totalBox = document.querySelector('.output p');
 var total = 0;
 list.innerHTML = '';
 totalBox.textContent = '';
-// number 1
+// number 1 - CS
 
 var products = ['Underpants:6.99','Socks:5.99','T-shirt:14.99', 'Trousers:31.99', 'Shoes:23.99'];
                 
 
 for (var i = 0; i < products.length; i++) { // number 2 - CS
-  // number 3 - CS 13 -> 18
-  var itemName = [];
-  var itemPrice = [];
-	
-  var split = products[i].split(".");
-  itemName.push(split[0]);
-  itemPrice.push(parseFloat(split[1])); 
+  // number 3 - CS 13 -> 16
 
-  // number 4
-  
-  // number 5
-  itemText = 0;
+  var split = products[i].split(":");
+  var itemName = split[0];
+  var itemPrice = parseFloat(split[1]);
+
+  // number 4 - CS
+  total += itemPrice
+
+  // number 5 - CS
+  itemText = itemName + " - " + "$" + itemPrice;
   
   var listItem = document.createElement('li');
   listItem.textContent = itemText;

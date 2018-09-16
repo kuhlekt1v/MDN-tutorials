9e972dc9b4ec12ca746f391ed4f48db09fa47e80
Cody S <sheridan.codyj@gmail.com>
2018-09-16 08:48:29 -0400

    Complete

--- 03A-sillyStoryGenerator/main.js
+++ 03A-sillyStoryGenerator/main.js
@@ -6,7 +6,7 @@ function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
 
-var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
@@ -27,13 +27,14 @@ function result() {
   
   if(customName.value !== '') {
     var name = customName.value;
-    newStory.replace('Bob', name);
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
     var weight = Math.round(300 * 0.071429) + ' stone';
     var temperature =  Math.round((94 - 32) * 0.5556) + ' centigrade';
-
+    newStory = newStory.replace('300 pounds', weight);
+    newStory = newStory.replace('94 farenheit', temperature);
   }
 
   story.textContent = newStory;

b85f2dadc77b31c70e106e8d4d14ce67c9a5b694
Cody S <sheridan.codyj@gmail.com>
2018-09-23 07:49:47 -0400

    Complete

--- 08-Image Gallery/main.js
+++ 08-Image Gallery/main.js
@@ -4,11 +4,31 @@ var thumbBar = document.querySelector('.thumb-bar');
 btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
-/* Looping through images */
-
+// Looping through images 
+for(i = 1; i <= 5; i++) {
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', displayImage);
+}
 
-/* Wiring up the Darken/Lighten button */
+// Display image
+function displayImage(e) {
+  displayedImage.setAttribute('src', e.target.getAttribute('src'));
+}
+
+// Wiring up the Darken/Lighten button
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+}
+
 
